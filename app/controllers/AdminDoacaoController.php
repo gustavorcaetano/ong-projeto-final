c718f4e1e52f79444b8aec03d2a22f9e20dc0122
ADM <adm@MacBook-Pro-de-ADM.local>
2026-09-15 09:31:56 -0300

    Adiciona cabecalhos CORS nos controllers para corrigir bloqueio de cross-origin

--- app/controllers/AdminDoacaoController.php
+++ app/controllers/AdminDoacaoController.php
@@ -1,6 +1,15 @@
 <?php
 use core\database\DBConnection;
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Responde imediatamente a requisições prévias do navegador (Preflight OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 header('Content-Type: application/json; charset=utf-8');
 $method = $_SERVER['REQUEST_METHOD'];
 
